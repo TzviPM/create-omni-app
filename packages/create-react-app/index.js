8c353aecd91e781d97bfc9faddffdb731fcf04ac
Tzvi Melamed <hartley.melamed@kandy.io>
2017-02-11 06:12:24 -0500

    Re-initialize create-omni-app

--- packages/create-react-app/index.js
+++ packages/create-react-app/index.js
@@ -1,33 +1,24 @@
 #!/usr/bin/env node
 
-/**
- * Copyright (c) 2015-present, Facebook, Inc.
- * All rights reserved.
- *
- * This source code is licensed under the BSD-style license found in the
- * LICENSE file in the root directory of this source tree. An additional grant
- * of patent rights can be found in the PATENTS file in the same directory.
- */
-
 // ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 //   /!\ DO NOT MODIFY THIS FILE /!\
 // ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 //
-// create-react-app is installed globally on people's computers. This means
+// create-omni-app is installed globally on people's computers. This means
 // that it is extremely difficult to have them upgrade the version and
 // because there's only one global version installed, it is very prone to
 // breaking changes.
 //
-// The only job of create-react-app is to init the repository and then
-// forward all the commands to the local version of create-react-app.
+// The only job of create-omni-app is to init the repository and then
+// forward all the commands to the local version of create-omni-app.
 //
 // If you need to add a new command, please add it to the scripts/ folder.
 //
 // The only reason to modify this file is to add more warnings and
-// troubleshooting information for the `create-react-app` command.
+// troubleshooting information for the `create-omni-app` command.
 //
 // Do not make breaking changes! We absolutely don't want to have to
-// tell people to update their global version of create-react-app.
+// tell people to update their global version of create-omni-app.
 //
 // Also be careful with new language features.
 // This file must work on Node 0.10+.
@@ -36,7 +27,7 @@
 //   /!\ DO NOT MODIFY THIS FILE /!\
 // ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
-'use strict';
+
 
 var chalk = require('chalk');
 
@@ -45,7 +36,7 @@ if (currentNodeVersion.split('.')[0] < 4) {
   console.error(
     chalk.red(
       'You are running Node ' + currentNodeVersion + '.\n' +
-      'Create React App requires Node 4 or higher. \n' +
+      'Create Omni App requires Node 4 or higher. \n' +
       'Please update your version of Node.'
     )
   );
@@ -69,19 +60,19 @@ var program = commander
     projectName = name;
   })
   .option('--verbose', 'print additional logs')
-  .option('--scripts-version <alternative-package>', 'use a non-standard version of react-scripts')
+  .option('--scripts-version <alternative-package>', 'use a non-standard version of omni-scripts')
   .allowUnknownOption()
   .on('--help', function () {
     console.log('    Only ' + chalk.green('<project-directory>') + ' is required.');
     console.log();
     console.log('    A custom ' + chalk.cyan('--scripts-version') + ' can be one of:');
-    console.log('      - a specific npm version: ' + chalk.green('0.8.2'));
-    console.log('      - a custom fork published on npm: ' + chalk.green('my-react-scripts'));
-    console.log('      - a .tgz archive: ' + chalk.green('https://mysite.com/my-react-scripts-0.8.2.tgz'));
+    console.log('      - a specific npm version: ' + chalk.green('0.1.0'));
+    console.log('      - a custom fork published on npm: ' + chalk.green('my-omni-scripts'));
+    console.log('      - a .tgz archive: ' + chalk.green('https://mysite.com/my-omni-scripts-0.1.0.tgz'));
     console.log('    It is not needed unless you specifically want to use a fork.');
     console.log();
     console.log('    If you have any problems, do not hesitate to file an issue:');
-    console.log('      ' + chalk.cyan('https://github.com/facebookincubator/create-react-app/issues/new'));
+    console.log('      ' + chalk.cyan('https://github.com/Omniroot/create-omni-app/issues/new'));
     console.log();
   })
   .parse(process.argv);
@@ -91,7 +82,7 @@ if (typeof projectName === 'undefined') {
   console.log('  ' + chalk.cyan(program.name()) + chalk.green(' <project-directory>'));
   console.log();
   console.log('For example:');
-  console.log('  ' + chalk.cyan(program.name()) + chalk.green(' my-react-app'));
+  console.log('  ' + chalk.cyan(program.name()) + chalk.green(' my-omni-app'));
   console.log();
   console.log('Run ' + chalk.cyan(program.name() + ' --help') + ' to see all options.');
   process.exit(1);
@@ -117,7 +108,7 @@ function createApp(name, verbose, version, template) {
   }
 
   console.log(
-    'Creating a new React app in ' + chalk.green(root) + '.'
+    'Creating a new Omni app in ' + chalk.green(root) + '.'
   );
   console.log();
 
@@ -134,7 +125,7 @@ function createApp(name, verbose, version, template) {
   process.chdir(root);
 
   console.log('Installing packages. This might take a couple minutes.');
-  console.log('Installing ' + chalk.cyan('react-scripts') + '...');
+  console.log('Installing ' + chalk.cyan('omni-scripts') + '...');
   console.log();
 
   run(root, appName, version, verbose, originalDirectory, template);
@@ -195,7 +186,7 @@ function run(root, appName, version, verbose, originalDirectory, template) {
 }
 
 function getInstallPackage(version) {
-  var packageToInstall = 'react-scripts';
+  var packageToInstall = 'omni-scripts';
   var validSemver = semver.valid(version);
   if (validSemver) {
     packageToInstall += '@' + validSemver;
@@ -209,7 +200,7 @@ function getInstallPackage(version) {
 // Extract package name from tarball url or path.
 function getPackageName(installPackage) {
   if (installPackage.indexOf('.tgz') > -1) {
-    // The package name could be with or without semver version, e.g. react-scripts-0.2.0-alpha.1.tgz
+    // The package name could be with or without semver version, e.g. omni-scripts-0.2.0-alpha.1.tgz
     // However, this function returns package name only without semver version.
     return installPackage.match(/^.+\/(.+?)(?:-\d+.+)?\.tgz$/)[1];
   } else if (installPackage.indexOf('@') > 0) {
@@ -235,7 +226,7 @@ function checkNodeVersion(packageName) {
     console.error(
       chalk.red(
         'You are running Node %s.\n' +
-        'Create React App requires Node %s or higher. \n' +
+        'Create Omni App requires Node %s or higher. \n' +
         'Please update your version of Node.'
       ),
       process.version,
@@ -248,7 +239,7 @@ function checkNodeVersion(packageName) {
 function checkAppName(appName) {
   // TODO: there should be a single place that holds the dependencies
   var dependencies = ['react', 'react-dom'];
-  var devDependencies = ['react-scripts'];
+  var devDependencies = ['omni-scripts'];
   var allDependencies = dependencies.concat(devDependencies).sort();
 
   if (allDependencies.indexOf(appName) >= 0) {
